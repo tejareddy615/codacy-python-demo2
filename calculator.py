9aa39681135b81ba873b233463dde7e7a17e4bfc
tejareddy615 <tejareddy615@gmail.com>
2025-07-14 16:11:49 -0500

    updated file name and code

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y): return x * y
+
+def divide(x, y):
+    if y == 0:
+        return None
+    return x / y
